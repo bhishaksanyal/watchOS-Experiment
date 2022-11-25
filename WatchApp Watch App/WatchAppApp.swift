e0bae45e02d69bc77ba2132e87f3fccbd9ff306f
bhishaksanyal <sanyalbishak93@gmail.com>
2022-11-25 16:33:43 +0530

    Initial project setup

---
+++ WatchApp Watch App/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp Watch App
+//
+//  Created by Bhishak Sanyal on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchApp/WatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchAppApp.swift
+//  WatchApp
+//
+//  Created by Bhishak Sanyal on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
